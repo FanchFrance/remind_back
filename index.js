3192b18e85d9df799895290e76fc413e8acbaced
bz699 <47868200+bz699@users.noreply.github.com>
2020-06-24 21:27:11 +0200

    Update index.js
    add localhost connection message

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const api = require("./routes");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use("/api", api);
+
+// test connexion
+app.get('/', (request, response) =>{
+    response.send('connection made');
+})
 
 app.listen(port, (err) => {
   if (err) {
